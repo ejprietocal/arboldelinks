eebdcef4384fe9856ab8d1752e8e72d8a6be42b3
ejprietocal <prietoeider1@gmail.com>
2024-04-18 21:37:50 -0500

    Quitandole el absolute a el footer

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,4 +1,4 @@
-<div class="container-fluid container-footer position-absolute bottom-0 d-flex align-items-center justify-content-center">
+<div class="container-fluid container-footer d-flex align-items-center justify-content-center">
     <div class="container mx-auto">
         <p class="mx-auto">Diseñado por <a href="https://pribyte.com/" target="_blank">Pribytes</a></p>
     </div>
